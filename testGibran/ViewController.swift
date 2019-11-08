559ba0502710d8af1942030a7b827d98314371be
Arminor <riosminor.andres@comunidad.unam.mx>
2019-11-08 17:12:41 -0600

    primer commit

---
+++ testGibran/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGibran
+//
+//  Created by Laboratorio UNAM-Apple 04 on 11/8/19.
+//  Copyright © 2019 unam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
